4a00dd93702c5d4c91104c32fff8332cef067ac7
ayslanmarcelino <ayslan_marcelino@outlook.com>
2019-10-17 11:55:24 -0300

    add all attributes in screen

--- src/view/movement.tsx
+++ src/view/movement.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import i18n from '../i18n';
 import RNPickerSelect from 'react-native-picker-select';
+import { Input } from 'react-native-elements';
 
 
 export default function SimpleBottomNavigation() {
@@ -19,6 +20,17 @@ export default function SimpleBottomNavigation() {
             { label: i18n.t('movement.recipe'), value: 'recipe' },
             { label: i18n.t('movement.transfer'), value: 'transfer' },
         ]}
+        style={pickerSelectStyles}
+      />
+      <Input
+        placeholder={i18n.t('movement.value')} 
+        leftIcon={{name:'monetization-on', color:'gray'}} 
+        inputContainerStyle={styles.input}
+        keyboardType={'numeric'}
+      />
+      <Button
+        title={i18n.t('movement.save')}
+        buttonStyle={styles.save}
       />
     </View>
   );
@@ -26,8 +38,46 @@ export default function SimpleBottomNavigation() {
 
 const styles = StyleSheet.create({
   view: {
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    paddingTop: 100,
     alignItems: 'center'
+  },
+  input: {
+    paddingTop: 20,
+    alignContent: 'center',
+    width: '50%',
+    alignSelf: 'center',
+    marginVertical: '1%',
+  },
+  save: {
+    borderRadius: 20,
+    width: '70%',
   }
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30,
+    alignSelf: 'center',
+    width: '70%'
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30,
+    alignSelf: 'center',
+    width: '70%'
+  },
 });
